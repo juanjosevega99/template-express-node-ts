b46bc473eea33c6639b4297c2570a705417b494b
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2022-11-21 23:10:34 -0500

    refactor: add status to route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,11 @@
-import { Application } from 'express'
+import { Application, Response } from 'express'
+import httpStatus from 'http-status'
 import os from 'os'
 
-export const router = (server: Application) => {
-  server.get('/', (_req, res) => {
-    res.send({
-      message: 'API Running',
-      instance: os.hostname()
-    })
+export const router = (server: Application): void => {
+  server.get('/', (_req, res: Response) => {
+    return res
+      .status(httpStatus.OK)
+      .send({ message: 'API Running', instance: os.hostname() })
   })
 }

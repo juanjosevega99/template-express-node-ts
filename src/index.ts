683a387b8476aa18cbbb67574172aac83ce9d603
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2022-11-22 08:15:37 -0500

    refactor: clean index and just set express

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 
-import { config } from './config/index'
 import { router } from './routes'
-import { Logger } from './utils/logger'
 
 const app = express()
 
@@ -11,9 +9,5 @@ app.use(cors())
 app.use(express.json())
 
 router(app)
-
-app.listen(config.port, () =>
-  Logger.info(`Server running at http://localhost:${config.port}`)
-)
 
 export default app
